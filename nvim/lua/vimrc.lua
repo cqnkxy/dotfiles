53cc39006d8518b2cbd021c4c16246bf055b515d
Yuan <cqnkxy@gmail.com>
2024-10-12 17:57:38 -0700

    Update vimrc.lua

--- nvim/lua/vimrc.lua
+++ nvim/lua/vimrc.lua
@@ -10,6 +10,23 @@ vim.cmd('colorscheme gruvbox')
 vim.api.nvim_set_keymap('n', '<leader>]', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>\\', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
 vim.g.NERDTreeIgnore = { '^bazel-.*', '^node_modules$' }
+vim.g.NERDTreeWinSize = '50'
+
+-- Google vim-codefmt
+vim.api.nvim_set_keymap('n', 'ff', ':FormatCode<CR>', { noremap = true, silent = true })
+-- Disable compatibility with vi
+vim.o.compatible = false
+
+-- Colorscheme
+vim.o.background = 'dark'
+vim.g.gruvbox_contrast_dark = 'hard'
+vim.cmd('colorscheme gruvbox')
+
+-- NERDTree
+vim.api.nvim_set_keymap('n', '<leader>]', ':NERDTreeToggle<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>\\', ':NERDTreeFocus<CR>', { noremap = true, silent = true })
+vim.g.NERDTreeIgnore = { '^bazel-.*', '^node_modules$' }
+vim.g.NERDTreeWinSize = '50'
 
 -- Google vim-codefmt
 vim.api.nvim_set_keymap('n', 'ff', ':FormatCode<CR>', { noremap = true, silent = true })
@@ -22,12 +39,12 @@ vim.api.nvim_create_autocmd('VimEnter', {
 })
 vim.g.rainbow_pairs = {{ '(', ')' }, { '[', ']' }, { '{', '}' }}
 
+-- telescope
+vim.api.nvim_set_keymap('n', '<C-P>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-]>', ':Telescope find_files<CR>', { noremap = true, silent = true })
+
 -- Airline theme
 vim.g.airline_theme = 'deus'
-
--- CtrlP
-vim.g.ctrlp_custom_ignore = 'node_modules\\|DS_Store\\|git\\|.cache\\|dist'
-vim.g.ctrlp_working_path_mode = 'r'
 
 -- Coq
 vim.g.coq_settings = {
@@ -61,3 +78,19 @@ vim.api.nvim_create_autocmd({'BufReadPost', 'FileReadPost'}, {
   pattern = '*',
   command = 'normal zR'
 })
+
+-- Treat visually wrapped line as a separate line when moving cursor
+vim.api.nvim_set_keymap('n', 'j', [[v:count ? 'j' : 'gj']], { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', 'k', [[v:count ? 'k' : 'gk']], { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', '$', [[v:count ? '$' : 'g$']], { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', '0', [[v:count ? '0' : 'g0']], { noremap = true, expr = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-J>', '<C-W><C-J>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-K>', '<C-W><C-K>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-L>', '<C-W><C-L>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-H>', '<C-W><C-H>', { noremap = true, silent = true })
+
+-- indent and eol
+vim.opt.list = true
+vim.opt.listchars:append "eol:↲"
+vim.opt.listchars:append "tab:| "
+vim.opt.listchars:append "space:."
