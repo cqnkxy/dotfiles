5614094fa97444f0c2ee28b007c158d91c2f9da7
Yuán <cqnkxy@gmail.com>
2026-04-25 19:08:14 -0700

    Auto reload buffer upon changes outside of neovim

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -37,6 +37,13 @@ vim.o.backspace = 'indent,eol,start'
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
+-- Auto-reload files modified outside Neovim
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
+
 -- Tabs & Indentation
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
